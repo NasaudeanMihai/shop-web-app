09801f73c7959e0b97de393b2497e3778c3118b8
mihai.nasaudean <mihai.nasaudean@devnest.ro>
2022-07-12 11:39:25 +0300

    added radio to the same component

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -2,18 +2,13 @@ import { FC, ReactElement } from 'react';
 import { RadioButtonProps } from './RadioButtonProps';
 import './radioButton.css';
 
-const RadioButton: FC<RadioButtonProps> = ({
-  handleRadioButtonOnChange,
-  item,
-  checked,
-}: RadioButtonProps): ReactElement => {
+const RadioButton: FC<RadioButtonProps> = ({ handleRadioButtonOnChange, item }: RadioButtonProps): ReactElement => {
   return (
     <div className="form-check row ">
       <input
         name={`${item.value}`}
         className="form-check-input input-style "
         type="radio"
-        checked={checked}
         onChange={event => handleRadioButtonOnChange(event)}
       />
       <p className="text-name">{item.name}</p>
--- src/components/RadioButton/RadioButtonProps.ts
+++ src/components/RadioButton/RadioButtonProps.ts
@@ -1,5 +1,4 @@
 export interface RadioButtonProps {
-  checked: boolean;
   item: { name: string; value: string | number };
   handleRadioButtonOnChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }

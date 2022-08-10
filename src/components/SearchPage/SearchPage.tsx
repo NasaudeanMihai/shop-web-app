831487309fa26ba7051a1e01c85c1287b4099bad
mihai.nasaudean <mihai.nasaudean@devnest.ro>
2022-08-10 16:53:31 +0300

    implement search

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -9,12 +9,15 @@ import ItemButton from '../Buttons/ItemButton/ItemButton';
 import Loading from '../Loader/Loading';
 import Filter from '../Filter/Filter';
 
+import './searchPage.css';
+
 const SearchPage: FC = (): ReactElement => {
   const { brand, category: categoryParams, price } = useParams();
   const navigate = useNavigate();
 
   const [dataList, setDataList] = useState<DataProps[]>([]);
   const [dataListFiltered, setDataListFiltered] = useState<DataProps[]>([]);
+  const [navbarColor, setNavbarColor] = useState<string>('transparent');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [category, setCategory] = useState<CategoryProps>({
     name: categoryParams ? categoryParams : '',
@@ -24,6 +27,15 @@ const SearchPage: FC = (): ReactElement => {
   const [selectedPriceFilter, setSelectedPriceFilter] = useState<Array<FilterItemBoxProps>>(
     price ? [{ name: price, value: parseInt(price) }] : [],
   );
+
+  const handleOnChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === '') {
+      setDataListFiltered(dataList);
+    } else {
+      const getFilteredData = dataList.filter(item => item.data.brand.startsWith(e.target.value));
+      setDataListFiltered(getFilteredData);
+    }
+  };
 
   const checkIsLoading = () => {
     if (isLoading) {
@@ -74,6 +86,17 @@ const SearchPage: FC = (): ReactElement => {
   }, [category, dataList, selectedBrandFilter, selectedPriceFilter]);
 
   useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 20) {
+        setNavbarColor('#D3D3D3');
+      }
+      if (window.scrollY <= 19) {
+        setNavbarColor('transparent');
+      }
+    });
+  }, []);
+
+  useEffect(() => {
     getFilteredData();
   }, [getFilteredData]);
 
@@ -90,8 +113,20 @@ const SearchPage: FC = (): ReactElement => {
   }, [category]);
 
   return (
-    <div className="wrapper-home-page container">
-      <div className="row" style={{ backgroundColor: 'transparent' }}>
+    <div className="container">
+      <div className="wrapper-search-bar" style={{ backgroundColor: navbarColor }}>
+        <div className="input-group mb-3 wrapper-input-search">
+          <input
+            onChange={e => handleOnChangeSearchInput(e)}
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+      </div>
+      <div className="row wrapper-content">
         <Filter
           setCategory={setCategory}
           category={category}
